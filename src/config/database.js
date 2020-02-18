52580820e6d430186444927906c6be8ddfdf9558
BillyStudy <billystudy@hotmail.com>
2020-02-18 18:10:05 -0300

    Config Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: "postgres",
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define:{
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  },
+};
+
+
