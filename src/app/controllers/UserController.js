ca29e5f8c7ae530fd2832d9182fe8d295978ddad
BillyStudy <billystudy@hotmail.com>
2020-02-19 15:09:49 -0300

    User update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,16 +5,29 @@ class UserController{
     const userExists = await User.findOne({ where: { email: req.body.email }})
     if (userExists) {
       return res.status(400).json({"error": "Email already exist"})
-
     }
     const {id , name , email, provider } = await User.create(req.body)
 
     return res.json({id , name , email, provider});
   }
 
-  async update(req, res){
-    console.log(req.userId);
-    return res.json({ok: true});
+  async update (req, res){
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email }});
+      if (userExists) {
+        return res.status(400).json({"error": "Email already exist"})
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({"error": "Password does not password "})
+    }
+
+    const {id, name, provider} = await user.update(req.body);
+    return res.json({id, name, email, provider});
   }
 }
 
