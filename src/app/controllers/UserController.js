be8239c8c34bb95ec14eea12c9cb800dc8e1d4f4
BillyStudy <billystudy@hotmail.com>
2020-02-19 15:56:54 -0300

    Validating input data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
 import User from '../models/User'
+import * as Yup from 'yup';
 
 class UserController{
   async store(req, res){
+    //Validação dos dados com Yup
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6)
+    });
+
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({"error": "Validation fails"})
+    }
+
+
+
     const userExists = await User.findOne({ where: { email: req.body.email }})
     if (userExists) {
       return res.status(400).json({"error": "Email already exist"})
@@ -12,8 +27,11 @@ class UserController{
   }
 
   async update (req, res){
+
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
+
+
 
     if (email !== user.email) {
       const userExists = await User.findOne({ where: { email }});
@@ -22,8 +40,25 @@ class UserController{
       }
     }
 
+    //Validação dos dados com Yup
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password_hash: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      confirmPassword: Yup.string().when('password_hash', (password_hash, field) =>
+      password_hash ? field.required().oneOf([Yup.ref('password_hash')]) : field
+      )
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({"error": "Validation fails"})
+    }
+
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(400).json({"error": "Password does not password "})
+      return res.status(400).json({"error": "Password does not match"})
     }
 
     const {id, name, provider} = await user.update(req.body);
