2973b3a1c9ff774268d26aca212f4dd3d3f2efb6
BillyStudy <billystudy@hotmail.com>
2020-02-26 18:55:03 -0300

    List available datetimes to appointments

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController'
+import AvailableController from './app/controllers/AvailableController'
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -21,7 +22,10 @@ const upload = multer(MulterConfig);
   routes.use(AuthMiddleware);
 
   routes.put('/users', UserController.update);
+
   routes.get('/providers', ProviderController.index);
+  routes.get('/providers/:providerId/available', AvailableController.index);
+
   routes.post('/files',upload.single('file'), FileController.store);
 
   routes.get('/schedule', ScheduleController.index);
